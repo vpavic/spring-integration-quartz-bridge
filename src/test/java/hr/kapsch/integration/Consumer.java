f12312551db43aa7d20ac347fe33c48e7c092c94
Vedran Pavic <vedran.pavic@gmail.com>
2016-06-03 08:57:20 +0200

    Update dependencies and improve build/tests

---
+++ src/test/java/hr/kapsch/integration/Consumer.java
@@ -0,0 +1,18 @@
+package hr.kapsch.integration;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Consumer {
+
+	private AtomicInteger counter = new AtomicInteger();
+
+	public void consumeMessage(String message) {
+		System.out.println(message);
+		this.counter.incrementAndGet();
+	}
+
+	public int consumedCount() {
+		return this.counter.intValue();
+	}
+
+}
---
+++ src/test/java/hr/kapsch/integration/IntegrationTest.java
@@ -0,0 +1,33 @@
+package hr.kapsch.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class IntegrationTest {
+
+	private static final int MESSAGE_COUNT = 10;
+
+	@Autowired
+	private MessageGateway gateway;
+
+	@Autowired
+	private Consumer consumer;
+
+	@Test
+	public void test() throws Exception {
+		for (int i = 0; i < MESSAGE_COUNT; i++) {
+			this.gateway.sendMessage("message-" + i);
+		}
+		Thread.sleep(MESSAGE_COUNT * 1000);
+		assertThat(this.consumer.consumedCount(), equalTo(MESSAGE_COUNT));
+	}
+
+}
